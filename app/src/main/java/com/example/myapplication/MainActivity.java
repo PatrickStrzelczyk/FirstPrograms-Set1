bb828ff1cfbb8d97aa7be7d055ec5ca9d5aa4448
Patrick Strzelczyk <bpatrick276@icloud.com>
2026-03-18 21:37:31 +0100

    Part B - Introduce Yourself

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText input = findViewById(R.id.inputName);
+        TextView output = findViewById(R.id.outputText);
+        Button button = findViewById(R.id.buttonWitaj);
+
+        button.setOnClickListener(v -> {
+            String text = input.getText().toString().trim();
+
+            if(!text.isEmpty()) {
+                output.setText("Witaj " + text);
+            } else {
+                output.setText("Przedstaw się.");
+            }
+        });
 
     }
 }
